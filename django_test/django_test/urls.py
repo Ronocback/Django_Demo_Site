ccf34b447592f9b283ecc385d7363955efa2459d
Conor Kerrigan <conor72@live.co.uk>
2020-06-18 17:34:41 +0100

    Added site admin functionality

--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from django.conf.urls import include, url
+from django.contrib   import admin
 from django_test import views 
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("quiz", include("quiz.urls"))
+    path("quiz", include("quiz.urls")),
+    url('admin/', admin.site.urls),
 ]
