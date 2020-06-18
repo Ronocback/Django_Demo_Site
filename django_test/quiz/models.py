02cd88295db23c6e67f791a4a79f05fa9d98049e
Conor Kerrigan <conor72@live.co.uk>
2020-06-18 14:44:02 +0100

    Added quiz model

--- django_test/quiz/models.py
+++ django_test/quiz/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    points = models.IntegerField()
+
+    def __str__(self):
+        return self.question_text
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    correct = models.BooleanField()
+
+    def __str__(self):
+        return self.choice_text
+
+
 # Create your models here.
