663ab38745e85580d4758e326afd7bf4953295dd
Conor Kerrigan <conor72@live.co.uk>
2020-07-11 12:57:47 +0100

    Added homepage view.

--- django_test/django_test/views.py
+++ django_test/django_test/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request, "django_test/index.html")
