c0a53963c322b667f2ccda0a5b460ce3a5192b86
Conor Kerrigan <conor72@live.co.uk>
2020-07-11 17:17:55 +0100

    Added css styling

--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from django.conf.urls import include, url
 from django.contrib   import admin
 from django_test import views 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.index, name="home"),
     path("quiz/", include("quiz.urls")),
     url('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- django_test/quiz/urls.py
+++ django_test/quiz/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
